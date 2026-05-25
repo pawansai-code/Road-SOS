4bfc82d38abd71d526b7b5ba7cd1c6d06b907024
pawansai-code <pawansairdj17@gmail.com>
2026-05-25 05:37:48 +0530

    removed the merge conflicct markers from the base.by from the backend folder and migrated the app using the command python manage.py migrate

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -31,6 +30,8 @@ INSTALLED_APPS = [
     'apps.users',
     'apps.services',
     'apps.emergency_contacts',
+    'apps.history',
+    'apps.sos',
 ]
 
 MIDDLEWARE = [
@@ -91,105 +92,3 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-=======
-# Base settings
-from pathlib import Path
-from decouple import config
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-
-SECRET_KEY = 'django-insecure-dummy-key-replace-in-prod'
-
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "*"
-]
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'rest_framework',
-    'corsheaders',
-
-    'apps.sos',
-]
-
-
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-
-ROOT_URLCONF = 'core.urls'
-
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-
-WSGI_APPLICATION = 'core.wsgi.application'
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASS'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
-
-
-AUTH_PASSWORD_VALIDATORS = []
-
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-STATIC_URL = 'static/'
-
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CORS_ALLOW_ALL_ORIGINS = True
->>>>>>> origin/sos-feature
